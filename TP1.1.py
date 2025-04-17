c88ec0a8092fad5268e020553f92fa22746d10d8
LuchoCsd <lucianocasado99@gmail.com>
2025-04-17 17:08:04 -0300

    initial commit

---
+++ TP1.1.py
@@ -0,0 +1,9 @@
+# El trabajo investigar consiste en construir una programa en lenguaje Python 3.x que simule el funcionamiento del plato
+# de una ruleta. Para esto se debe tener en cuenta lo siguientes temas:
+# • Generación de valores aleatorios enteros.
+# • Uso de listas para el almacenamiento de datos.
+# • Uso de la estructura de control FOR para iterar las listas.
+# • Empleo de funciones estadísticas.
+# • Gráficas de los resultados mediante el paquete Matplotlib.
+# • Ingreso por consola de parámetros para la simulación (cantidad de tiradas, corridas y número elegido, Ejemplo
+#   python -c XXX -n YYY -e ZZ).
